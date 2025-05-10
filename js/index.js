61af6e586ca798ee03a03707ecb86dc6fbf627f4
Mylea Spicer <mylea2012@gmail.com>
2025-05-10 12:07:37 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -20,3 +20,49 @@ skills.forEach((skillName) => {
   skill.textContent = skillName;
   skillsList.appendChild(skill);
 });
+
+const messageForm = document.getElementsByName("leave_message")[0];
+// Add an event listener for the "submit" event
+messageForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submission
+  const userName = event.target.usersName.value;
+  const userEmail = event.target.usersEmail.value;
+  const userMessage = event.target.usersMessage.value;
+  // Log the values to the console for verification
+  console.log("Name:", userName);
+  console.log("Email:", userEmail);
+  console.log("Message:", userMessage);
+
+  // Select the #messages section by id
+  const messageSection = document.querySelector("#messages");
+  // Query the messageSection to find the <ul> element
+  const messageList = messageSection.querySelector("ul");
+
+  // Create a new list item element
+  const newMessage = document.createElement("li");
+
+  // Set the inner HTML of the newMessage element
+  newMessage.innerHTML = ` <a href="mailto:${userEmail}">${userName}</a>
+    <span>: ${userMessage}</span>`;
+
+  // Create a new button element
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "remove";
+  removeButton.type = "button";
+
+  // Add an event listener to the removeButton
+  removeButton.addEventListener("click", function () {
+    // Find the button's parent element
+    const entry = removeButton.parentNode;
+    // Remove the entry element from the DOM
+    entry.remove();
+  });
+
+  newMessage.appendChild(removeButton);
+
+  // Append the new message to the messageList
+  messageList.appendChild(newMessage);
+
+  // Clear the form
+  messageForm.reset();
+});
